91e34619f142d6620c8ad5b86c97383b73f95cc7
Michael Mendoza <mm1246@student.london.ac.uk>
2024-01-05 05:01:08 +0800

    refactor: WeatherPage component to use WeatherPageTable

--- src/screens/Weather/WeatherPage.tsx
+++ src/screens/Weather/WeatherPage.tsx
@@ -1,14 +1,6 @@
 import React, { useState, useEffect } from "react";
 import style from "./WeatherPage.module.scss";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { kelvinToFahrenheit } from "../../utils/TemperatureConverter";
-import { formatDate } from "../../utils/dateFormatter";
+import WeatherPageTable from "./WeatherPageTable";
 
 interface WeatherPageProps {
   city: string;
@@ -69,40 +61,7 @@ const WeatherPage: React.FC<WeatherPageProps> = ({ city }) => {
   return (
     <div className={style.container}>
       <h1>Weather in {city}</h1>
-      {weatherData && (
-        <TableContainer component={Paper}>
-          <Table aria-label="weather data table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Date (mm/dd/yyyy)</TableCell>
-                <TableCell align="right">Temp (°F)</TableCell>
-                <TableCell align="right">Description</TableCell>
-                <TableCell align="right">Main</TableCell>
-                <TableCell align="right">Pressure (hPa)</TableCell>
-                <TableCell align="right">Humidity (%)</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {weatherData.list.map((item, index) => (
-                <TableRow key={index}>
-                  <TableCell component="th" scope="row">
-                    {formatDate(item.dt)}
-                  </TableCell>
-                  <TableCell align="right">
-                    {kelvinToFahrenheit(item.main.temp).toFixed(2)}
-                  </TableCell>
-                  <TableCell align="right">
-                    {item.weather[0].description}
-                  </TableCell>
-                  <TableCell align="right">{item.weather[0].main}</TableCell>
-                  <TableCell align="right">{item.main.pressure}</TableCell>
-                  <TableCell align="right">{item.main.humidity}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      )}
+      {weatherData && <WeatherPageTable weatherData={weatherData} />}
     </div>
   );
 };
