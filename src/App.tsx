fdcf1521b01cc25481bfd5868e2e9b7deffa72a3
Michael Mendoza <mm1246@student.london.ac.uk>
2024-01-05 08:46:11 +0800

    style: Add container styles and import module in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,27 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./screens/Landing/LandingPage";
 import HomePage from "./screens/Home/HomePage";
 import WeatherPage from "./screens/Weather/WeatherPage";
+import style from "./App.module.scss";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route
-          path="/home"
-          element={
-            <HomePage
-              userName="User Name"
-              userGithubUrl="https://github.com/username"
-            />
-          }
-        />
-        <Route path="/weather/:city" element={<WeatherPage />} />
-      </Routes>
-    </BrowserRouter>
+    <div className={style.container}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route
+            path="/home"
+            element={
+              <HomePage
+                userName="User Name"
+                userGithubUrl="https://github.com/username"
+              />
+            }
+          />
+          <Route path="/weather/:city" element={<WeatherPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 };
 
