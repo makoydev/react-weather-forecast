ff34e6c6e3d10e1f6d18e72a0166faefca44a43a
Michael Mendoza <mm1246@student.london.ac.uk>
2024-01-05 15:03:46 +0800

    feat: Add ProtectedRoute component

---
+++ src/screens/ProtectedRoute.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Navigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+
+interface ProtectedRouteProps {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  component: React.ComponentType<any>; // Allow any component with any props
+  [key: string]: any; // Allow any additional props
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
+  component: Component,
+  ...props
+}) => {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return isAuthenticated ? <Component {...props} /> : <Navigate to="/" />;
+};
+
+export default ProtectedRoute;
