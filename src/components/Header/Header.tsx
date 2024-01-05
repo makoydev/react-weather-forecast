463cafcaf7653ad5e5aaa73ffab9b39ebf8e2b39
Michael Mendoza <mm1246@student.london.ac.uk>
2024-01-05 16:30:35 +0800

    refactor: Header component styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,8 @@
-// Header.js or Header.tsx
-
 import { useAuth0 } from "@auth0/auth0-react";
-import { useNavigate, useLocation } from "react-router-dom";
 import style from "./Header.module.scss"; // Ensure your styles are set up correctly
 
 const Header = () => {
   const { isAuthenticated, logout } = useAuth0();
-  const navigate = useNavigate();
-  const location = useLocation();
 
   const handleLogout = () => {
     logout({
@@ -17,21 +12,10 @@ const Header = () => {
     });
   };
 
-  const handleBack = () => {
-    navigate("/home");
-  };
-
-  const isWeatherPage = location.pathname.includes("/weather/"); // Checks if the pathname includes '/weather/'
-
   return (
     <header className={style.header}>
       <div className={style.logo}>Weather App</div>
       <nav className={style.navigation}>
-        {isWeatherPage && (
-          <button className={style.backBtn} onClick={handleBack}>
-            Back
-          </button>
-        )}
         {isAuthenticated && (
           <button className={style.logoutBtn} onClick={handleLogout}>
             Logout
