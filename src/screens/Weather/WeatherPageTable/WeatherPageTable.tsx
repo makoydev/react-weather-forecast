2bf6862522515512888af4f1727efa1e5a9ead70
Michael Mendoza <mm1246@student.london.ac.uk>
2024-01-05 16:31:27 +0800

    style: Add responsive styling to WeatherPageTable

--- src/screens/Weather/WeatherPageTable/WeatherPageTable.tsx
+++ src/screens/Weather/WeatherPageTable/WeatherPageTable.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -6,9 +6,9 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import TablePagination from "@mui/material/TablePagination";
 import { formatDate } from "../../../utils/dateFormatter";
 import { kelvinToFahrenheit } from "../../../utils/temperatureConverter";
+import style from "./WeatherPageTable.module.scss"; // Ensure you have the SCSS file
 
 interface WeatherData {
   list: {
@@ -27,70 +27,55 @@ interface WeatherPageTableProps {
 }
 
 const WeatherPageTable: React.FC<WeatherPageTableProps> = ({ weatherData }) => {
-  const rowsPerPageOptions = [10]; // Options for rows per page
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(rowsPerPageOptions[0]);
-
-  const handleChangePage = (_: unknown, newPage: number) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0); // Reset to the first page
-  };
-
-  const rowsToShow = weatherData.list.slice(
-    page * rowsPerPage,
-    page * rowsPerPage + rowsPerPage
-  );
+  const firstItem = weatherData.list[0];
 
   return (
-    <>
-      <TableContainer component={Paper}>
-        <Table aria-label="weather data table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Date (mm/dd/yyyy)</TableCell>
-              <TableCell align="right">Temp (°F)</TableCell>
-              <TableCell align="right">Description</TableCell>
-              <TableCell align="right">Main</TableCell>
-              <TableCell align="right">Pressure (hPa)</TableCell>
-              <TableCell align="right">Humidity (%)</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rowsToShow.map((item, index) => (
-              <TableRow key={index}>
-                <TableCell component="th" scope="row">
-                  {formatDate(item.dt)}
-                </TableCell>
-                <TableCell align="right">
-                  {kelvinToFahrenheit(item.main.temp).toFixed(2)}
-                </TableCell>
-                <TableCell align="right">
-                  {item.weather[0].description}
-                </TableCell>
-                <TableCell align="right">{item.weather[0].main}</TableCell>
-                <TableCell align="right">{item.main.pressure}</TableCell>
-                <TableCell align="right">{item.main.humidity}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={rowsPerPageOptions}
-        component="div"
-        count={weatherData.list.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </>
+    <TableContainer component={Paper}>
+      <Table aria-label="weather data table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Date (mm/dd/yyyy)</TableCell>
+            <TableCell align="right">Temp (°F)</TableCell>
+            {/* Add class names for hiding in mobile view */}
+            <TableCell align="right" className={style.hideOnMobile}>
+              Description
+            </TableCell>
+            <TableCell align="right" className={style.hideOnMobile}>
+              Main
+            </TableCell>
+            <TableCell align="right" className={style.hideOnMobile}>
+              Pressure (hPa)
+            </TableCell>
+            <TableCell align="right" className={style.hideOnMobile}>
+              Humidity (%)
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell component="th" scope="row">
+              {formatDate(firstItem.dt)}
+            </TableCell>
+            <TableCell align="right">
+              {kelvinToFahrenheit(firstItem.main.temp).toFixed(2)}
+            </TableCell>
+            {/* Hide these cells on mobile */}
+            <TableCell align="right" className={style.hideOnMobile}>
+              {firstItem.weather[0].description}
+            </TableCell>
+            <TableCell align="right" className={style.hideOnMobile}>
+              {firstItem.weather[0].main}
+            </TableCell>
+            <TableCell align="right" className={style.hideOnMobile}>
+              {firstItem.main.pressure}
+            </TableCell>
+            <TableCell align="right" className={style.hideOnMobile}>
+              {firstItem.main.humidity}
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
