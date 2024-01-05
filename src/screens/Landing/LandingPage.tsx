e4c6be9fd130e6e91e48e91ae65f30ca08c33cb0
Michael Mendoza <mm1246@student.london.ac.uk>
2024-01-05 15:16:48 +0800

    style: LandingPage styles and import LoginButton component

--- src/screens/Landing/LandingPage.tsx
+++ src/screens/Landing/LandingPage.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import style from "./LandingPage.module.scss";
-import LoginButton from "../../components/Buttons/Login";
+import LoginButton from "../../components/Buttons/Login/Login";
 
 const LandingPage: React.FC = () => {
   return (
     <div className={style.container}>
-      <h1>
+      <h3>
         Welcome to the weather forecast web application. Please login with your
         Github user to use the application and view the weather in your city.
-      </h1>
+      </h3>
       <LoginButton />
     </div>
   );
